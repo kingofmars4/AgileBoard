63402049bc66e674f5fdef32e392a8817999afd7
David Rodrigues <55930667+kingofmars4@users.noreply.github.com>
2025-09-14 18:56:15 +0100

    Auto mapping swagger

--- AgileBoard.API/Program.cs
+++ AgileBoard.API/Program.cs
@@ -111,8 +111,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "AgileBoard API v1");
-        c.RoutePrefix = string.Empty;
+        c.RoutePrefix = "swagger";
     });
+    app.MapGet("/", () => Results.Redirect("/swagger"));
 }
 
 app.UseHttpsRedirection();
